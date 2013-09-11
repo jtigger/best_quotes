931279f59a3040d86a6b6d1162584248ed48bc3e
John S. Ryan <jtigger@infosysengr.com>
2013-09-11 14:12:20 -0700

    Now references dependency on Rulers via :path and makes use of autoloading of controllers.

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -3,6 +3,11 @@
 class QuotesController < Rulers::Controller
   def a_quote
     "There is nothing either good or bad " +
-     "only thinking makes it so."
+     "only thinking makes it so." +
+    "\n<pre>\n#{env}\n</pre>"
+  end
+  
+  def exception
+    raise "doh!"
   end
 end
